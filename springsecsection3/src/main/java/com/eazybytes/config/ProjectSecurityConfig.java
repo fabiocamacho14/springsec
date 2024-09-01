fee79017411a3ef1b9727fb2abeadc3bd042dbfc
fabiocamacho14 <fabiocamacho14@hotmail.com>
2024-09-01 00:01:59 -0300

    section 3 end

--- springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -2,8 +2,19 @@ package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,8 +30,29 @@ public class ProjectSecurityConfig {
                     .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                     .requestMatchers("/notices", "/contact", "/error").permitAll();
         });
-        http.formLogin(flc -> flc.disable());
+//        http.formLogin(flc -> flc.disable());
+        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$rcN9Y3prF2zUpRuD6w0CVOiblSUV5mGBYRS5.ExoT4crSFwNVZA7m")
+                .authorities("admin").build();
+
+         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
